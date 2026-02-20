8927936129432f34f1cafeb6eea0e3afa86d799f
aaaroz <ram.ardiansyah18@gmail.com>
2026-02-20 22:57:11 +0700

    chore: change arabic to uthmanic

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.06qn3p02bq4"
+    "revision": "0.dr4ld0rkhpg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
